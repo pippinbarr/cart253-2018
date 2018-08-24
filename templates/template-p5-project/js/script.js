c4180386d03eb69f66207e908852d31889b06f41
Pippin Barr <pippin.barr@gmail.com>
2018-08-24 10:37:19 -0400

    Added template p5 project and downloadable zip

---
+++ templates/template-p5-project/js/script.js
@@ -0,0 +1,26 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
